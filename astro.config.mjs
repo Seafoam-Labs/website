523e5ef7479ce5287f68258c6da5cc01a62a496b
Anton Ždanov <anton@azdanov.dev>
2026-05-03 16:16:25 +0300

    feat(config): enable auto sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.seafoam-labs.org",
   integrations: [
     starlight({
       title: "Seafoam Labs",
