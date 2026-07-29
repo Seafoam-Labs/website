500b25930ba2c392bc15183ff672cfe79e637023
Anton Ždanov <anton@azdanov.dev>
2026-07-29 06:25:17 +0300

    Move CLI reference into autogenerate docs for shelly

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,24 +5,10 @@ import { changelogsLoader } from "starlight-changelogs/loader";
 import { rssSchema } from "@astrojs/rss";
 import { glob } from "astro/loaders";
 import { docsLoader } from "@astrojs/starlight/loaders";
-import { remoteMarkdownLoader } from "./loaders/remote-markdown";
 import { remoteTextLoader } from "./loaders/remote-text";
 
 export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-  remoteDocs: defineCollection({
-    loader: remoteMarkdownLoader([
-      {
-        id: "shelly-cli-reference",
-        title: "Shelly CLI Reference",
-        url: "https://raw.githubusercontent.com/Seafoam-Labs/Shelly-ALPM/refs/heads/master/wiki/cli_help.md",
-      },
-    ]),
-    schema: z.object({
-      title: z.string(),
-      sourceUrl: z.url(),
-    }),
-  }),
   remoteConfigs: defineCollection({
     loader: remoteTextLoader([
       {
